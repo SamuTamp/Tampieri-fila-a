f339b65ae9c1a8716ef6993ae5286394ed7745c1
tampieri_samuele <tampieri.samuele@iisgalvanimi.edu.it>
2026-02-06 10:37:30 +0000

    terzo punto non completato

--- Tampieri-fila-a/src/app/app.ts
+++ Tampieri-fila-a/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SecondComponent } from './second-component/second-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SecondComponent], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Tampieri-fila-a');
+  title = ('Tampieri-fila-a');
+  Tampieri = 'app';
 }
